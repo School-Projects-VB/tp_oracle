c5b37e3f1daa8349546409ebb85f3b7d5483993e
VBetsch <vic.betsch@gmail.com>
2022-11-21 15:31:19 +0100

    ✨ Create view

--- view.sql
+++
@@ -1,4 +0,0 @@
-SELECT C.category_id, C.category_name, SUM(P.produit_price) AS category_sum_product_price, SUM(P.produit_stock) AS category_sum_product_stock
-FROM CATEGORIES C
-INNER JOIN PRODUITS P on C.CATEGORY_ID = P.PRODUIT_CATEGORY
-GROUP BY C.CATEGORY_ID, C.CATEGORY_NAME
