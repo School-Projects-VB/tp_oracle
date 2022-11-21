7f37d0790f76f3ee22c67dd29362834f634fa3b5
VBetsch <vic.betsch@gmail.com>
2022-11-21 15:02:24 +0100

    ✅ Add data test

--- data.sql
+++ data.sql
@@ -1,4 +1,18 @@
+-- CATEGORIES
+INSERT INTO CATEGORIES (category_name)
+VALUES ('iphone');
+
+INSERT INTO CATEGORIES (category_name)
+VALUES ('iphone +');
+
+-- PRODUITS
 INSERT INTO PRODUITS (produit_name, produit_category, produit_price, produit_stock)
-VALUES ('iphone 13', 10, 958, 10);
+VALUES ('iphone 13', 1, 958, 10);
+
+INSERT INTO PRODUITS (produit_name, produit_category, produit_price, produit_stock)
+VALUES ('iphone X', 1, 1050, 250);
+
+INSERT INTO PRODUITS (produit_name, produit_category, produit_price, produit_stock)
+VALUES ('iphone 13', 2, 2000, 500);
 
 COMMIT WORK;
